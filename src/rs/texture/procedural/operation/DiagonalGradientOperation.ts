ebaa94590c978628e360993d868eef3b0fae37ef
tritao <joao@tritao.eu>
2026-02-02 01:32:33 +0000

    refactor: name DiagonalGradientOperation params

--- src/rs/texture/procedural/operation/DiagonalGradientOperation.ts
+++ src/rs/texture/procedural/operation/DiagonalGradientOperation.ts
@@ -3,9 +3,11 @@ import { TextureGenerator } from "../TextureGenerator";
 import { TextureOperation } from "./TextureOperation";
 
 export class DiagonalGradientOperation extends TextureOperation {
-    interpolationMode = 0;
-    steepness = 1;
-    mixMode = 0;
+    // 0: diagonal distance (x - y), 1: radial distance from center
+    distanceMode = 0;
+    // 0: sine, 1: sawtooth (raw phase), 2: triangle
+    waveformMode = 0;
+    frequency = 1;
 
     constructor() {
         super(0, true);
@@ -13,11 +15,11 @@ export class DiagonalGradientOperation extends TextureOperation {
 
     override decode(field: number, buffer: ByteBuffer): void {
         if (field === 0) {
-            this.mixMode = buffer.readUnsignedByte();
+            this.distanceMode = buffer.readUnsignedByte();
         } else if (field === 1) {
-            this.interpolationMode = buffer.readUnsignedByte();
+            this.waveformMode = buffer.readUnsignedByte();
         } else if (field === 3) {
-            this.steepness = buffer.readUnsignedByte();
+            this.frequency = buffer.readUnsignedByte();
         }
     }
 
@@ -27,30 +29,31 @@ export class DiagonalGradientOperation extends TextureOperation {
         }
         const output = this.monochromeImageCache.get(line);
         if (this.monochromeImageCache.dirty) {
-            const inParam = textureGenerator.verticalGradient[line];
-            const nParam = (inParam - 2048) >> 1;
+            const y = textureGenerator.verticalGradient[line];
+            const yCenteredHalf = (y - 2048) >> 1;
             for (let pixel = 0; pixel < textureGenerator.width; pixel++) {
-                const inValue = textureGenerator.horizontalGradient[pixel];
-                const nValue = (inValue - 2048) >> 1;
-                let pytN: number;
-                if (this.mixMode === 0) {
-                    pytN = (inValue - inParam) * this.steepness;
+                const x = textureGenerator.horizontalGradient[pixel];
+                const xCenteredHalf = (x - 2048) >> 1;
+                let phaseQ12: number;
+                if (this.distanceMode === 0) {
+                    phaseQ12 = (x - y) * this.frequency;
                 } else {
-                    const sqNSum = (nParam * nParam + nValue * nValue) >> 12;
-                    pytN = (4096.0 * Math.sqrt(sqNSum / 4096.0)) | 0;
-                    pytN = (this.steepness * pytN * 3.141592653589793) | 0;
+                    const radiusSqQ12 =
+                        (yCenteredHalf * yCenteredHalf + xCenteredHalf * xCenteredHalf) >> 12;
+                    phaseQ12 = (4096.0 * Math.sqrt(radiusSqQ12 / 4096.0)) | 0;
+                    phaseQ12 = (this.frequency * phaseQ12 * 3.141592653589793) | 0;
                 }
-                pytN -= pytN & ~0xfff;
-                if (this.interpolationMode === 0) {
-                    pytN = (TextureGenerator.SINE[(pytN >> 4) & 0xff] + 4096) >> 1;
-                } else if (this.interpolationMode === 2) {
-                    pytN -= 2048;
-                    if (pytN < 0) {
-                        pytN = -pytN;
+                phaseQ12 -= phaseQ12 & ~0xfff;
+                if (this.waveformMode === 0) {
+                    phaseQ12 = (TextureGenerator.SINE[(phaseQ12 >> 4) & 0xff] + 4096) >> 1;
+                } else if (this.waveformMode === 2) {
+                    phaseQ12 -= 2048;
+                    if (phaseQ12 < 0) {
+                        phaseQ12 = -phaseQ12;
                     }
-                    pytN = (2048 - pytN) << 1;
+                    phaseQ12 = (2048 - phaseQ12) << 1;
                 }
-                output[pixel] = pytN;
+                output[pixel] = phaseQ12;
             }
         }
         return output;
