265a224ae8fc3e3e65ecc957ec4b9837b5d823d1
tritao <joao@tritao.eu>
2026-02-03 00:19:36 +0000

    refactor(compression): remove unused Gzip.web and dead initWasm

--- src/rs/compression/Gzip.ts
+++ src/rs/compression/Gzip.ts
@@ -1,8 +1,6 @@
 import gzip from "gzip-js";
 
 export class Gzip {
-    static async initWasm(): Promise<void> {}
-
     static decompress(compressed: Uint8Array): Int8Array {
         const decompressed = new Int8Array(gzip.unzip(compressed));
         return decompressed;
