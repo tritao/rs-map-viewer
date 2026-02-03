753b096e016cbbfe579283f54f1a679888ffd405
tritao <joao@tritao.eu>
2026-02-03 09:56:47 +0000

    cache(store): drop unused SectorCluster.decode

--- src/rs/cache/store/SectorCluster.ts
+++ src/rs/cache/store/SectorCluster.ts
@@ -1,13 +1,5 @@
-import { ByteBuffer } from "../../io/ByteBuffer";
-
 export class SectorCluster {
     static readonly SIZE: i32 = 6;
-
-    static decode(buffer: ByteBuffer): SectorCluster {
-        const size = buffer.readMedium();
-        const sector = buffer.readMedium();
-        return new SectorCluster(size, sector);
-    }
 
     constructor(
         readonly size: i32,
