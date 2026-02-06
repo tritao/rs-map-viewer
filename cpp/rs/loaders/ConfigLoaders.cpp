f962de2c079b58fa153e9e17b3374035fc5cc3a9
tritao <joao@tritao.eu>
2026-02-06 22:38:02 +0000

    port(cpp): wire more config loaders into ConfigLoaders

--- cpp/rs/loaders/ConfigLoaders.cpp
+++ cpp/rs/loaders/ConfigLoaders.cpp
@@ -7,12 +7,21 @@
 #include "../core/Result.hpp"
 #include "../core/Status.hpp"
 #include "../types.hpp"
+#include "../config/bastype/BasTypeLoader.hpp"
 #include "../config/enumtype/EnumTypeLoader.hpp"
 #include "../config/floortype/FloorTypeLoaders.hpp"
+#include "../config/invtype/InvTypeLoader.hpp"
 #include "../config/idktype/IdkTypeLoader.hpp"
+#include "../config/loctype/LocTypeLoader.hpp"
+#include "../config/mapscenetype/MapSceneTypeLoader.hpp"
+#include "../config/meltype/MapElementTypeLoader.hpp"
+#include "../config/npctype/NpcTypeLoader.hpp"
+#include "../config/objtype/ObjTypeLoader.hpp"
 #include "../config/paramtype/ParamTypeLoader.hpp"
+#include "../config/questtype/QuestTypeLoader.hpp"
 #include "../config/seqtype/SeqTypeLoader.hpp"
 #include "../config/spotanimtype/SpotAnimTypeLoader.hpp"
+#include "../config/structtype/StructTypeLoader.hpp"
 #include "../config/vartype/VarBitTypeLoader.hpp"
 
 namespace rs {
@@ -21,11 +30,50 @@ static constexpr i32 DAT2_INDEX_CONFIGS = 2;
 static constexpr i32 DAT2_CONFIG_ARCHIVE_UNDERLAYS = 1;
 static constexpr i32 DAT2_CONFIG_ARCHIVE_IDENTKITS = 3;
 static constexpr i32 DAT2_CONFIG_ARCHIVE_OVERLAYS = 4;
+static constexpr i32 DAT2_CONFIG_ARCHIVE_INV = 5;
+static constexpr i32 DAT2_CONFIG_ARCHIVE_LOCS = 6;
 static constexpr i32 DAT2_CONFIG_ARCHIVE_ENUMS = 8;
+static constexpr i32 DAT2_CONFIG_ARCHIVE_NPCS = 9;
+static constexpr i32 DAT2_CONFIG_ARCHIVE_OBJS = 10;
 static constexpr i32 DAT2_CONFIG_ARCHIVE_PARAMS = 11;
 static constexpr i32 DAT2_CONFIG_ARCHIVE_SEQS = 12;
 static constexpr i32 DAT2_CONFIG_ARCHIVE_SPOTANIMS = 13;
 static constexpr i32 DAT2_CONFIG_ARCHIVE_VARBITS = 14;
+
+static constexpr i32 RS2_CONFIG_ARCHIVE_BAS = 32;
+static constexpr i32 OSRS_CONFIG_ARCHIVE_STRUCT = 34;
+static constexpr i32 RS2_CONFIG_ARCHIVE_MAPSCENES = 34;
+static constexpr i32 OSRS_CONFIG_ARCHIVE_MAPFUNCTIONS = 35;
+static constexpr i32 RS2_CONFIG_ARCHIVE_QUESTS = 35;
+static constexpr i32 RS2_CONFIG_ARCHIVE_MAPFUNCTIONS = 36;
+
+template <typename Loader>
+static Status loadOptionalArchiveLoader(
+    const CacheSystem& cacheSystem,
+    const CacheInfo& cacheInfo,
+    Allocator& alloc,
+    i32 indexId,
+    i32 archiveId,
+    Loader* out) noexcept {
+    if (!out) {
+        return Status::InvalidArgument;
+    }
+    auto archRes = cacheSystem.getArchive(indexId, archiveId, alloc);
+    if (!archRes.isOk()) {
+        if (archRes.status() == Status::NotFound) {
+            *out = Loader{};
+            return Status::Ok;
+        }
+        return archRes.status();
+    }
+    const Archive archive = rs::move(archRes.value());
+    auto r = Loader::fromArchive(cacheInfo, archive, alloc);
+    if (!r.isOk()) {
+        return r.status();
+    }
+    *out = rs::move(r.value());
+    return Status::Ok;
+}
 
 Result<ConfigLoaders> tryCreateConfigLoaders(const CacheSystem& cacheSystem, const CacheInfo& cacheInfo, Allocator& alloc) noexcept {
     const CacheType cacheType = detectCacheType(cacheInfo);
@@ -81,6 +129,21 @@ Result<ConfigLoaders> tryCreateConfigLoaders(const CacheSystem& cacheSystem, con
     }
 
     {
+        // Dat2: inv types are stored in configs index (2), archive id 5.
+        auto archRes = cacheSystem.getArchive(DAT2_INDEX_CONFIGS, DAT2_CONFIG_ARCHIVE_INV, alloc);
+        if (!archRes.isOk()) {
+            return Result<ConfigLoaders>::err(archRes.status());
+        }
+        const Archive archive = rs::move(archRes.value());
+
+        auto r = InvTypeLoader::fromArchive(cacheInfo, archive, alloc);
+        if (!r.isOk()) {
+            return Result<ConfigLoaders>::err(r.status());
+        }
+        out.invs = rs::move(r.value());
+    }
+
+    {
         // Dat2: enums are stored in configs index (2), archive id 8.
         auto archRes = cacheSystem.getArchive(DAT2_INDEX_CONFIGS, DAT2_CONFIG_ARCHIVE_ENUMS, alloc);
         if (!archRes.isOk()) {
@@ -93,6 +156,51 @@ Result<ConfigLoaders> tryCreateConfigLoaders(const CacheSystem& cacheSystem, con
             return Result<ConfigLoaders>::err(r.status());
         }
         out.enums = rs::move(r.value());
+    }
+
+    {
+        // Dat2: loc types are stored in configs index (2), archive id 6.
+        auto archRes = cacheSystem.getArchive(DAT2_INDEX_CONFIGS, DAT2_CONFIG_ARCHIVE_LOCS, alloc);
+        if (!archRes.isOk()) {
+            return Result<ConfigLoaders>::err(archRes.status());
+        }
+        const Archive archive = rs::move(archRes.value());
+
+        auto r = LocTypeLoader::fromArchive(cacheInfo, archive, alloc);
+        if (!r.isOk()) {
+            return Result<ConfigLoaders>::err(r.status());
+        }
+        out.locs = rs::move(r.value());
+    }
+
+    {
+        // Dat2: npc types are stored in configs index (2), archive id 9.
+        auto archRes = cacheSystem.getArchive(DAT2_INDEX_CONFIGS, DAT2_CONFIG_ARCHIVE_NPCS, alloc);
+        if (!archRes.isOk()) {
+            return Result<ConfigLoaders>::err(archRes.status());
+        }
+        const Archive archive = rs::move(archRes.value());
+
+        auto r = NpcTypeLoader::fromArchive(cacheInfo, archive, alloc);
+        if (!r.isOk()) {
+            return Result<ConfigLoaders>::err(r.status());
+        }
+        out.npcs = rs::move(r.value());
+    }
+
+    {
+        // Dat2: obj types are stored in configs index (2), archive id 10.
+        auto archRes = cacheSystem.getArchive(DAT2_INDEX_CONFIGS, DAT2_CONFIG_ARCHIVE_OBJS, alloc);
+        if (!archRes.isOk()) {
+            return Result<ConfigLoaders>::err(archRes.status());
+        }
+        const Archive archive = rs::move(archRes.value());
+
+        auto r = ObjTypeLoader::fromArchive(cacheInfo, archive, alloc);
+        if (!r.isOk()) {
+            return Result<ConfigLoaders>::err(r.status());
+        }
+        out.objs = rs::move(r.value());
     }
 
     {
@@ -153,6 +261,38 @@ Result<ConfigLoaders> tryCreateConfigLoaders(const CacheSystem& cacheSystem, con
             return Result<ConfigLoaders>::err(r.status());
         }
         out.varBits = rs::move(r.value());
+    }
+
+    // Optional extras: archive ids vary by game type, but we can pick safely for the primary cases.
+    {
+        const Status s = loadOptionalArchiveLoader(cacheSystem, cacheInfo, alloc, DAT2_INDEX_CONFIGS, RS2_CONFIG_ARCHIVE_BAS, &out.bas);
+        if (!ok(s)) {
+            return Result<ConfigLoaders>::err(s);
+        }
+    }
+
+    if (cacheInfo.game == GameType::Oldschool) {
+        const Status sStruct = loadOptionalArchiveLoader(cacheSystem, cacheInfo, alloc, DAT2_INDEX_CONFIGS, OSRS_CONFIG_ARCHIVE_STRUCT, &out.structs);
+        if (!ok(sStruct)) {
+            return Result<ConfigLoaders>::err(sStruct);
+        }
+        const Status sMf = loadOptionalArchiveLoader(cacheSystem, cacheInfo, alloc, DAT2_INDEX_CONFIGS, OSRS_CONFIG_ARCHIVE_MAPFUNCTIONS, &out.mapFunctions);
+        if (!ok(sMf)) {
+            return Result<ConfigLoaders>::err(sMf);
+        }
+    } else {
+        const Status sScenes = loadOptionalArchiveLoader(cacheSystem, cacheInfo, alloc, DAT2_INDEX_CONFIGS, RS2_CONFIG_ARCHIVE_MAPSCENES, &out.mapScenes);
+        if (!ok(sScenes)) {
+            return Result<ConfigLoaders>::err(sScenes);
+        }
+        const Status sQuests = loadOptionalArchiveLoader(cacheSystem, cacheInfo, alloc, DAT2_INDEX_CONFIGS, RS2_CONFIG_ARCHIVE_QUESTS, &out.quests);
+        if (!ok(sQuests)) {
+            return Result<ConfigLoaders>::err(sQuests);
+        }
+        const Status sMf = loadOptionalArchiveLoader(cacheSystem, cacheInfo, alloc, DAT2_INDEX_CONFIGS, RS2_CONFIG_ARCHIVE_MAPFUNCTIONS, &out.mapFunctions);
+        if (!ok(sMf)) {
+            return Result<ConfigLoaders>::err(sMf);
+        }
     }
 
     return Result<ConfigLoaders>::ok(rs::move(out));
--- cpp/rs/loaders/ConfigLoaders.hpp
+++ cpp/rs/loaders/ConfigLoaders.hpp
@@ -10,9 +10,18 @@
 #include "../config/floortype/FloorTypeLoaders.hpp"
 #include "../config/idktype/IdkTypeLoader.hpp"
 #include "../config/enumtype/EnumTypeLoader.hpp"
+#include "../config/invtype/InvTypeLoader.hpp"
+#include "../config/loctype/LocTypeLoader.hpp"
+#include "../config/npctype/NpcTypeLoader.hpp"
+#include "../config/objtype/ObjTypeLoader.hpp"
 #include "../config/paramtype/ParamTypeLoader.hpp"
 #include "../config/seqtype/SeqTypeLoader.hpp"
 #include "../config/spotanimtype/SpotAnimTypeLoader.hpp"
+#include "../config/bastype/BasTypeLoader.hpp"
+#include "../config/questtype/QuestTypeLoader.hpp"
+#include "../config/mapscenetype/MapSceneTypeLoader.hpp"
+#include "../config/meltype/MapElementTypeLoader.hpp"
+#include "../config/structtype/StructTypeLoader.hpp"
 
 namespace rs {
 
@@ -20,11 +29,22 @@ struct ConfigLoaders {
     VarBitTypeLoader varBits;
     UnderlayFloorTypeLoader underlays;
     OverlayFloorTypeLoader overlays;
+    InvTypeLoader invs;
     IdkTypeLoader identKits;
+    LocTypeLoader locs;
     EnumTypeLoader enums;
+    NpcTypeLoader npcs;
+    ObjTypeLoader objs;
     ParamTypeLoader params;
     SeqTypeLoader seqs;
     SpotAnimTypeLoader spotAnims;
+
+    // Optional / revision-dependent config archives (loaded when present).
+    BasTypeLoader bas;
+    QuestTypeLoader quests;
+    MapSceneTypeLoader mapScenes;
+    MapElementTypeLoader mapFunctions;
+    StructTypeLoader structs;
 };
 
 // Minimal starting point for the config/loaders port. Extend this struct as we port more config types.
